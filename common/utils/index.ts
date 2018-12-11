681e948374c10ba7d3288b8f01d405247b506faf
sokaman <skmdev29@gmail.com>
2018-12-12 07:46:54 +0800

    Improve coding style

--- common/utils/index.ts
+++ common/utils/index.ts
@@ -1,3 +1,11 @@
 export function isServer() {
   return typeof window === 'undefined';
 }
+
+export const padNumber = (n) => (n < 10 ? `0${n}` : n);
+
+export function formatTime(t) {
+  return `${padNumber(t.getUTCHours())}:${padNumber(
+    t.getUTCMinutes()
+  )}:${padNumber(t.getUTCSeconds())}`;
+}
