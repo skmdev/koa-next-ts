4898d7465c8bfe318114206b9719492ede947ce8
sokaman <skmdev29@gmail.com>
2018-12-14 22:00:00 +0800

    Remove json and change to ts

--- config/index.ts
+++ config/index.ts
@@ -6,13 +6,13 @@ let config: IConfig;
 
 switch (process.env.NODE_ENV) {
   case 'development':
-    config = require('./config.dev.json');
+    config = require('./config.dev');
     break;
   case 'production':
-    config = require('./config.prod.json');
+    config = require('./config.prod');
     break;
   default:
-    config = require('./config.default.json');
+    config = require('./config.default');
     break;
 }
 
